875529852c67a79d59c6dbf32a05fa732b3d7fc7
Andrew Chung <aandrewchung@gmail.com>
2023-09-13 14:30:12 -0400

    added andrewid query & respective test case

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -1,9 +1,17 @@
 export default function QueryProcessor(query: string): string {
-  if (query.toLowerCase().includes("shakespeare")) {
+  const lowerQuery = query.toLowerCase();
+
+  if (lowerQuery.includes("shakespeare")) {
     return (
       "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
       "English poet, playwright, and actor, widely regarded as the greatest " +
       "writer in the English language and the world's pre-eminent dramatist."
+    );
+  }
+
+  if (lowerQuery.includes("andrew id")) {
+    return (
+      "Your Andrew ID is awchung"
     );
   }
 
