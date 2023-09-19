73b1080152ea3eb7e045714f7ab42ab85854e359
Andrew Chung <aandrewchung@gmail.com>
2023-09-19 16:05:19 -0400

    added 3 types of queries

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -21,5 +21,72 @@ export default function QueryProcessor(query: string): string {
     );
   }
 
+  if (query.match(/Which of the following numbers is the largest:\s*(\d+)\s*,\s*(\d+)\s*,\s*(\d+)\s*\?/)) {
+
+    const match = query.match(/Which of the following numbers is the largest:\s*(\d+)\s*,\s*(\d+)\s*,\s*(\d+)\s*\?/);
+
+    if (match) {
+      const num1 = parseInt(match[1]);
+      const num2 = parseInt(match[2]);
+      const num3 = parseInt(match[3]);
+      
+      const largest = Math.max(num1, num2, num3);
+      
+      return largest.toString();
+    } else {
+      return "No valid query found.";
+    }
+  }
+
+  
+
+  if (query.match(/What is (\d+) plus (\d+)\?/)) {
+
+    const match = query.match(/What is (\d+) plus (\d+)\?/);
+
+    if (match) {
+      const num1 = parseInt(match[1]);
+      const num2 = parseInt(match[2]);
+    
+      const result = num1 + num2;
+    
+      return result.toString();
+    } else {
+      return "Invalid format. Please provide a valid addition query.";
+    }
+    
+  }
+
+  if (query.match(/Which of the following numbers is both a square and a cube:\s*((?:\d+\s*,\s*)+\d+)\s*\?/)) {
+
+    const match = query.match(/Which of the following numbers is both a square and a cube:\s*((?:\d+\s*,\s*)+\d+)\s*\?/);
+
+    if (match) {
+      const numbersString = match[1];
+      const numbers = numbersString.split(',').map(Number);
+    
+      // Find numbers that are both squares and cubes
+      const resultNumbers = numbers.filter(num => {
+        const squareRoot = Math.sqrt(num);
+        const cubeRoot = Math.cbrt(num);
+        return squareRoot === Math.floor(squareRoot) && cubeRoot === Math.floor(cubeRoot);
+      });
+    
+      if (resultNumbers.length > 0) {
+        return resultNumbers.join(', ');
+      } else {
+        return "None of the numbers are both a square and a cube.";
+      }
+    } else {
+      return "Invalid format. Please provide a valid query.";
+    }
+    
+    
+  } 
+
+  
+
+
+
   return "";
 }
